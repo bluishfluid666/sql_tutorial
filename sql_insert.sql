bccbac854ff704d580f93e8bb419f86f60737cb9
bluishfluid666 <nguyenmanhkhang0805@gmail.com>
2022-07-28 16:35:54 +0700

    Learn SQL Insert

---
+++ sql_insert.sql
@@ -0,0 +1,14 @@
+-- Insert a new record in the Customers table
+INSERT INTO Customers(
+  CustomerName,
+  Addr,
+  City,
+  PostalCode,
+  Country,
+) VALUES (
+  'Hekkan Burger',
+  'Gateveien 15',
+  'Sandnes',
+  '4306',
+  'Norway'
+)
