6387ad2dba33b0afa07edbfa438c1410453266a5
bluishfluid666 <nguyenmanhkhang0805@gmail.com>
2022-08-02 11:00:50 +0700

    Delete duplicate emails in an example table.

--- delete_dupl_emails.sql
+++ delete_dupl_emails.sql
@@ -1,4 +1,22 @@
 -- Active: 1631701382289@@127.0.0.1@3306@world
+/*
+Person table:
++----+------------------+
+| id | email            |
++----+------------------+
+| 1  | john@example.com |
+| 2  | bob@example.com  |
+| 3  | john@example.com |
++----+------------------+
+Output: 
++----+------------------+
+| id | email            |
++----+------------------+
+| 1  | john@example.com |
+| 2  | bob@example.com  |
++----+------------------+
+Explanation: john@example.com is repeated two times. We keep the row with the smallest Id = 1.
+*/
 Create table If Not Exists Person (Id int, Email varchar(255));
 Truncate table Person;
 insert into Person (id, email) values ('1', 'john@example.com');
